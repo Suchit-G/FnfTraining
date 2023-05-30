71c0717922e5c2bd67524a24634b1d17fc07f750
Suchit <128065956+Suchit-G@users.noreply.github.com>
2023-05-30 17:56:23 +1200

    Delete ChekoutApplication directory

--- ChekoutApplication/Models/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChekoutApplication.Models;
-
-public class Item
-{
-    public int Price { get; set; }
-    public int OfferQuantity { get; set; }
-    public int OfferPrice { get; set; }
-
-}
